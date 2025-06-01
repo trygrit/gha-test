d0ef3fb62b76465f35015924c6e1936d86ace0c9
Allan Dalton <allan@helloallan.com>
2025-06-01 20:36:57 +1000

    ai refactor

--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -5,12 +5,12 @@ import (
 	"text/template"
 )
 
-//go:embed **.tmpl
+//go:embed *.tmpl
 var templates embed.FS
 
 func LoadTemplates() (*template.Template, error) {
 	// Parse all templates in the embedded filesystem
-	tmpl, err := template.ParseFS(templates, "**.tmpl")
+	tmpl, err := template.ParseFS(templates, "*.tmpl")
 	if err != nil {
 		return nil, err
 	}
