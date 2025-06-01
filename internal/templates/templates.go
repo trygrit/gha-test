0a2507bcf648bf357feacdb98ed90d8ba3204c15
Allan Dalton <allan@helloallan.com>
2025-06-01 20:41:35 +1000

    fix: improve template loading and error handling

--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -2,6 +2,7 @@ package templates
 
 import (
 	"embed"
+	"fmt"
 	"text/template"
 )
 
@@ -10,9 +11,22 @@ var templates embed.FS
 
 func LoadTemplates() (*template.Template, error) {
 	// Parse all templates in the embedded filesystem
-	tmpl, err := template.ParseFS(templates, "*.tmpl")
-	if err != nil {
-		return nil, err
+	tmpl := template.New("")
+
+	// List of required templates
+	requiredTemplates := []string{"plan_success.tmpl", "plan_failure.tmpl", "general.tmpl"}
+
+	for _, name := range requiredTemplates {
+		content, err := templates.ReadFile(name)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read template %s: %v", name, err)
+		}
+
+		_, err = tmpl.New(name).Parse(string(content))
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse template %s: %v", name, err)
+		}
 	}
+
 	return tmpl, nil
 }
